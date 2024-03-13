da07b2abd8367aa6fbc68a9097c210ed40a2a522
CatalinaRpoS <catrestreposa@unal.edu.co>
2024-03-12 20:49:13 -0500

    Cleaning data

--- pregunta.py
+++ pregunta.py
@@ -6,15 +6,38 @@ Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizad
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
+
 import pandas as pd
 
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
 
-    #
-    # Inserte su código aquí
-    #
+    df = df.replace("-", " ", regex=True).replace("_", " ", regex=True)
+
+    # Limpieza de las columnas "sexo", "tipo_de_emprendimiento" y "línea_credito"
+    df["sexo"] = df["sexo"].str.lower()
+    df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].str.lower()
+
+    # Limpieza de las columnas "idea_negocio" y "barrio"
+    df["idea_negocio"] = df["idea_negocio"].str.lower().str.strip()
+    df["barrio"] = df["barrio"].str.lower().str.strip()
+    df["línea_credito"] = df["línea_credito"].str.lower().str.strip()
+
+    # Conversión a entero de la columna "comuna_ciudadano"
+    df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int)
+
+    # Limpieza del formato de la columna "fecha_de_beneficio"
+    df["fecha_de_beneficio"] = pd.to_datetime(
+        df.fecha_de_beneficio, format="mixed"
+    ).dt.strftime("%d/%m/%Y")
+
+    # Limpieza de la columna "monto_del_credito"
+    df["monto_del_credito"] = df["monto_del_credito"].replace("[,$]", "", regex=True)
+    df["monto_del_credito"] = df["monto_del_credito"].replace("(.00$)", "", regex=True)
+
+    # Eliminación de columnas duplicadas y nulas
+    df = df.drop_duplicates().dropna()
 
     return df
